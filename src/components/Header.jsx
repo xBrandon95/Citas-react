e822b7eaba35fc06f0084cb62f882779352159ae
Brandon Ticona <jbticona4@gmail.com>
2022-01-25 18:35:10 -0400

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <h1 className="font-black text-5xl text-center mt-14 md:w-2/3 mx-auto mb-10">
-      Seguimiento Pacientes {''}
+      Seguimiento de Pacientes {''}
       <span className="text-indigo-600">Veterinaria</span>
     </h1>
   );
